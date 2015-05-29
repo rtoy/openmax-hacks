1f7266881ca68ccb9898d4da6ee41a7e169392b9
Raymond Toy <rtoy@chromium.org>
2015-05-29 13:43:24 -0700

    Fix two issues in printing the results.
    o When verbose = 0, just print out the SNR value without annotation.
    o In PrintResultNoSNR, add a newline when printing with verbose = 0.

--- dl/sp/src/test/test_fft_time.c
+++ dl/sp/src/test/test_fft_time.c
@@ -459,7 +459,7 @@ void PrintShortHeader(const char* message) {
 void PrintResult(const char* prefix, int fft_log_size, double elapsed_time,
                  int count, double snr) {
   if (verbose == 0) {
-    printf("%2d\t%8.4f\t%8d\t%.4e # SNR = %g\n",
+    printf("%2d\t%8.4f\t%8d\t%.4e\t%g\n",
            fft_log_size, elapsed_time, count, 1000 * elapsed_time / count, snr);
   } else {
     printf("%-18s:  order %2d:  %8.4f sec for %8d FFTs:  %.4e msec/FFT # SNR = %g\n",
@@ -470,9 +470,9 @@ void PrintResult(const char* prefix, int fft_log_size, double elapsed_time,
 }
 
 void PrintResultNoSNR(const char* prefix, int fft_log_size, double elapsed_time,
-                 int count) {
+                      int count) {
   if (verbose == 0) {
-    printf("%2d\t%8.4f\t%8d\t%.4e",
+    printf("%2d\t%8.4f\t%8d\t%.4e\n",
            fft_log_size, elapsed_time, count, 1000 * elapsed_time / count);
   } else {
     printf("%-18s:  order %2d:  %8.4f sec for %8d FFTs:  %.4e msec/FFT\n",
